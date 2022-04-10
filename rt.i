3d9eb88ef112d2facb135705628ff130d32dd577
Alex Wade <38112687+adw62@users.noreply.github.com>
2022-04-10 15:27:19 +0100

    Add files via upload

---
+++ rt.i
@@ -0,0 +1,41 @@
+%module rt
+
+%{
+    #define SWIG_FILE_WITH_INIT 
+%}
+
+%include "numpy.i"
+
+%init %{
+    import_array();
+%}
+
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* cameras, int d1, int f1)}
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* lights, int d2, int f2)}
+
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* obj_pos, int obj_num1, int d3)}
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* obj_amb, int obj_num2, int d4)}
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* obj_diff, int obj_num3, int d5)}
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* obj_spec, int obj_num4, int d6)}
+%apply (float* IN_ARRAY2, int DIM1, int DIM2) {(float* pix_loc, int n1, int d7)}
+
+%apply (float* IN_ARRAY1, int DIM1) {(float* obj_size, int obj_num5)}
+%apply (float* IN_ARRAY1, int DIM1) {(float* obj_shine, int obj_num6)}
+%apply (float* IN_ARRAY1, int DIM1) {(float* obj_refl, int obj_num7)}
+
+%apply (float* INPLACE_ARRAY2, int DIM1, int DIM2) {(float* pixels, int n, int m)}
+
+%inline %{
+void rt(float *cameras, int d1, int f1, float *lights, int d2, int f2,
+                float *obj_pos, int obj_num1, int d3,
+                float *obj_amb, int obj_num2, int d4,
+                float *obj_diff, int obj_num3, int d5,
+                float *obj_spec, int obj_num4, int d6,
+                float *obj_size, int obj_num5,
+                float *obj_shine, int obj_num6,
+                float *obj_refl, int obj_num7,
+                float *pixels, int n, int m,
+                float *pix_loc, int n1, int d7);
+%}
+
+
---
+++ run.py
@@ -0,0 +1,54 @@
+from rt import rt
+import numpy as np
+import matplotlib.pyplot as plt
+
+obj_amb = np.array([[0.1, 0, 0],
+                    [0.1, 0, 0.1],
+                    [0, 0.1, 0],
+                    [0.1, 0.1, 0.1]], 'f')
+obj_diff = np.array([[0.7, 0, 0],
+                     [0.7, 0.0, 0.7],
+                     [0, 0.6, 0],
+                     [0.6, 0.6, 0.6]], 'f')
+obj_spec = np.array([[1, 1, 1],
+                     [1, 1, 1],
+                     [1, 1, 1],
+                     [1, 1, 1]], 'f')
+obj_size = np.array([0.7,
+                     0.1,
+                     0.15,
+                     9000 - 0.7], 'f')
+obj_shine = np.array([100,
+                      100,
+                      100,
+                      100], 'f')
+obj_refl = np.array([0.5,
+                     0.5,
+                     0.5,
+                     0.5], 'f')
+pixels = np.zeros([1572864, 3], 'f')
+x = 1536
+y = 1024
+
+pix_loc = np.array([[i, j] for i in np.linspace(-1, 1, x) for j in np.linspace(-1, 1, y)], 'f')
+obj_pos = np.array([[-0.2, 0, -1],
+                    [0.1, -0.3, 0],
+                    [-0.3, 0, 0],
+                    [0, -9000, 0]], 'f')
+cameras = np.array([[0, 0, 1]], 'f')
+lights = np.array([[5, 5, i] for i in np.linspace(0, 5, 100)], 'f')
+for i, light in enumerate(lights):
+    print(i)
+    if i < 25:
+        obj_pos[0][0] += 0.1
+        obj_pos[0][1] += 0.01
+    else:
+        obj_pos[0][0] -= 0.1
+    rt(cameras, np.array([light]), obj_pos, obj_amb, obj_diff, obj_spec, obj_size, obj_shine, obj_refl, pixels, pix_loc)
+    pixels = np.clip(pixels, 0, 1)
+    pixels.shape = (x, y, 3)
+    plt.imsave(f'./img/{i:03}_img.png', pixels)
+    pixels = pixels*0.0
+    pixels.shape = (1572864, 3)
+
+
